027335b64fea2883cb5274eb72a3d8d0376a5ad9
Finemas <proovaznik@gmail.com>
2020-02-13 16:21:13 +0100

    Add red bottom border on edit [s6]

--- zScanner/SupportFiles/Extensions/UITextField.swift
+++ zScanner/SupportFiles/Extensions/UITextField.swift
@@ -19,4 +19,17 @@ extension UITextField {
         self.layer.shadowOpacity = 1.0
         self.layer.shadowRadius = 0.0
     }
+    
+    #warning("TODO Maybe we can merge these two methods")
+    func addBottomBorder() {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: self.frame.size.height - 1, width: self.frame.size.width, height: 2)
+        bottomLine.backgroundColor = UIColor.red.cgColor
+        borderStyle = .none
+        layer.addSublayer(bottomLine)
+    }
+    
+    func removeBottomBorder() {
+        self.layer.sublayers?.removeAll()
+    }
 }
