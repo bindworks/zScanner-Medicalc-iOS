2335556e16ee14ba485f140b53a41622ee1d57ce
Jan Provazník <proovaznik@gmail.com>
2020-03-03 18:08:12 +0100

    Update view controller extension

--- zScanner/SupportFiles/Extensions/UIViewController.swift
+++ zScanner/SupportFiles/Extensions/UIViewController.swift
@@ -20,7 +20,7 @@ extension UIViewController {
         view.addGestureRecognizer(tap)
     }
     
-    @objc func dismissKeyboard() {
+    @objc private func dismissKeyboard() {
         view.endEditing(true)
     }
 }
